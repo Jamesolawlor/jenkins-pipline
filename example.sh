ea57ca8e5206e9843ec93304feabb1d549856d8b
Jamesolawlor <62955601+Jamesolawlor@users.noreply.github.com>
2024-03-07 10:52:08 +0000

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,18 +1,1 @@
-pipeline {
-    agent any
-
-    stages {
-        
-        stage('Run Script) {
-            steps{
-                sh 'chmod +x ./example.sh'
-                sh './example.sh'
-                
-            }
-        }
-        
-        
-    }
-    
-    
-}
+echo "Hello World"
